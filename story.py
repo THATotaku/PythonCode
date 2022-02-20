e857c1ead770e9b6e764374ca8b7d9adc599eea0
THATotaku <82127230+THATotaku@users.noreply.github.com>
2022-02-20 17:54:39 +1300

    General update to story.py
    - grammar, spelling, punctuation, slight sentence tweak.
    - Remove name asking as that is to be moved to character.py 
    - Made it easier to say yes here too : 3

--- story.py
+++ story.py
@@ -1,17 +1,15 @@
 def Story():
     print("Welcome and thanks for reading and playing! This is my first project so I know it can be crude and even might not be fun to play, but who cares!\n"
-            "Let's get to the actual game now :)\n"
-            "To begin the journey please write your name!")
-    userName=str(input("What is your name?"))
-    print("So your",userName,"...\n"
-            "Okay, might as well fill you in on what missed. It seems that you have missed quite a lot, not sure how... but you did. Basically, the entire land of Algoraea has been thrusted into a civil war!\n"
+            "Let's get to the actual game now :)\n")
+    print("Okay, *INSERT NAME HERE SOONTM* might as well fill you in on what you missed. It seems that you have missed quite a lot, not sure how... but you did. Basically, the entire land of Algoraea has been thrust into a civil war!\n"
             "However, this isn't just some civil war that can be culled through diplomatic means, no, this can only be calmed through the complete decimation of all those who 'oppose(d)' the old king.\n" 
-            "The proud old king, Yovlosh, in a fit of rage and pure delusion, declared that any subject who has ancestral blood ties to dwarves, elves, and anything of like.\n"
+            "The proud old king, Yovlosh, in a fit of rage and pure delusion, declared that any subject who has ancestral blood ties to dwarves, elves, and anything of the like.\n"
             #Yovlosh is pronounced like YO-veL-aw-Sch, old king whom has extreme race prejudice and is the entire founder of the civil war 
-            "are to be expunged for their tainted blood. As to be expected, those who were loyal to the king obeyed his rule, but even those who were soon to be ostricized and murdered for just existing\n"
-            "were starting to fall in line with the decree, simply out of fear and hope that if they were to submit to a lesser life of slavehood, woodland and mountain living, and through the extreme process of being sterilized through magic\n"
-            "I am unsure of how you have escaped this tragedy so far, but I fear that you will be soon thrusted into this civil war. No one comes out of these sorts of things unscathed, and especially those who look as innocent as you.\n"
-            "by the way...", end=" ")
+            "are to be expunged for their tainted blood. As to be expected, those who were loyal to the king obeyed his rule, but even those who were soon to be ostracized and murdered for just existing\n"
+            "were starting to fall in line with the decree, simply out of fear and hope that if they were to submit to a lesser life of slavehood, woodland and mountain living, and through the extreme process of being\n"
+            "sterilized through magic. I am unsure of how you have escaped this tragedy so far, but I fear that you will be soon thrust into this civil war. No one comes out of these sorts of things unscathed, and\n"
+            "especially those who look as innocent as you.\n"
+            "By the way...", end=" ")
     birthPlace=str(input("Where are you from?"))
     birthPlaceRacialName=(birthPlace+(str("ian")))
     algoraeaRacialName=(str("Algoraean"))#The Entirety of the continent, those who reside are considered this, but bigger city-states are this OR their city populous
@@ -19,14 +17,14 @@ def Story():
     saphkGatesCityState=(str("The Saphks"))#Huge southern coast trade empire. A city state that deals in black market deals, slave trade, weapons deals, etc. 
         
 
-    print("What! I don't believe you! How can you possibly be from", birthPlace,"?!\n"
-    "It's been almost 200 years since someone has laid eyes on someone from", birthPlace,"\n"
-    "It was said that through the beginning years of the civil war that the mages of", birthPlace,"were wiped out by the Orcs that were enslaved by the kings men\n"
-    "I... I can't believe that I.. me.. an", algoraeaRacialName, "would be the first person since the beginning of the civil war to see an",birthPlaceRacialName)
-    print("Now, that you are up to snuff,\n")
+    print("What?! I don't believe you! How can you possibly be from", birthPlace,"?!\n"
+    "It's been almost 200 years since anyone has laid eyes on someone from", birthPlace,".\n"
+    "It was said that through the beginning years of the civil war that the mages of", birthPlace,"were wiped out by the Orcs that were enslaved by the kings men.\n"
+    "I... I can't believe that I.. me.. an", algoraeaRacialName, "would be the first person since the beginning of the civil war to see an",birthPlaceRacialName,"...")
+    print("Well, anyway, now that you're up to snuff,\n")
     toContinue=str(input("Are you ready to continue? Yes or no?"))
-    if toContinue in ['y', 'yes', 'Yes']:
-        print("Ah good, now we can actually get goin', my feet started to hurt from just standin' around")
+    if toContinue.lower() in ['y', "yes", "sure", "ok", "okay", "ye", "yep"]:
+        print("Ah good, now we can *actually* get goin', my feet were starting to fall asleep from just standin' around")
         print("First question\n"
             "This is also where the options are laid out\n"
             "a)...\n"
